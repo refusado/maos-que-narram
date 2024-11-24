b87056f4c074423b10d161c232759621fdd77b2b
refusado <refusado@gmail.com>
2024-11-24 05:27:21 -0300

    sistema de perguntas

--- web/src/app/conteudo/story-screen/index.tsx
+++ web/src/app/conteudo/story-screen/index.tsx
@@ -1,35 +1,55 @@
 import { Story } from '@/types';
 import Link from 'next/link';
+import { VideoPlayer } from './video-player';
+import { VideoPlayerControls } from './video-player copy';
+import { QuestionsContainer } from './questions-container';
 
 interface StoryScreenProps {
   story: Story;
 }
 
 export function StoryScreen({ story }: StoryScreenProps) {
+  const { title, description, video_url, questions, category } = story;
+
   return (
     <>
-      <section
-        id="infos"
-        className="container px-4 py-20 sm:px-6 md:py-12 lg:px-8"
-      >
+      <section id="infos" className="container px-4 py-8">
         <p className="mb-2">
           <Link href="/" className="text-blue-600 hover:underline">
             Voltar
           </Link>
         </p>
-        <h1 className="block text-3xl font-bold text-neutral-800 lg:text-5xl">
-          {story.title}
-        </h1>
-        <p>{story.description}</p>
+        <h1 className="text-3xl lg:text-5xl">{title}</h1>
+        <p>{description}</p>
       </section>
 
-      <section id="video" className="container px-4 py-8 sm:px-6 lg:px-8">
-        <p>[vídeo aqui...]</p>
+      <section
+        id="video"
+        className="container flex flex-col gap-4 px-4 py-4 md:gap-8 lg:flex-row lg:gap-10"
+      >
+        <VideoPlayer src={video_url} />
+        <VideoPlayerControls />
       </section>
 
-      <section id="perguntas" className="container px-4 py-8 sm:px-6 lg:px-8">
-        <p>[questões aqui...]</p>
-      </section>
+      {questions && questions.length > 0 && (
+        <section id="perguntas" className="container px-4 py-8 pb-20">
+          <h2 className="mb-2 text-2xl lg:text-3xl">
+            Perguntas relacionadas à história
+          </h2>
+          <p className="mb-10">
+            Responda a estas perguntas para testar o seu nível de entendimento
+            na história <b>{title}</b>!
+          </p>
+
+          <QuestionsContainer questions={questions} />
+
+          <p className="mt-6">
+            <Link href="/" className="text-blue-600 hover:underline">
+              Voltar para o início
+            </Link>
+          </p>
+        </section>
+      )}
     </>
   );
 }
---
+++ web/src/app/conteudo/story-screen/questions-container.tsx
@@ -0,0 +1,111 @@
+'use client';
+
+import { useState } from 'react';
+import { Question } from '@/types';
+
+interface QuestionsContainerProps {
+  questions: Question[];
+}
+
+export function QuestionsContainer({ questions }: QuestionsContainerProps) {
+  const [answers, setAnswers] = useState<{ [key: number]: boolean | null }>({});
+  const [selectedOptions, setSelectedOptions] = useState<{
+    [key: number]: string;
+  }>({});
+
+  const handleAnswer = (
+    questionIndex: number,
+    isCorrect: boolean,
+    selectedOption: string,
+  ) => {
+    setAnswers((prev) => ({
+      ...prev,
+      [questionIndex]: isCorrect,
+    }));
+
+    setSelectedOptions((prev) => ({
+      ...prev,
+      [questionIndex]: selectedOption,
+    }));
+  };
+
+  return (
+    <div className="space-y-6">
+      {questions.map((question, index) => (
+        <div
+          key={index}
+          className="rounded-2xl bg-neutral-100 p-3.5 shadow-sm lg:p-6"
+        >
+          <h2 className="mb-4 text-lg font-semibold">
+            {index + 1}. {question.question}
+          </h2>
+          <ul className="space-y-2">
+            {question.options.map((option, optionIndex) => (
+              <QuestionOption
+                key={optionIndex}
+                questionIndex={index}
+                optionIndex={optionIndex}
+                optionText={option.text}
+                isCorrect={option.is_correct}
+                isSelected={selectedOptions[index] === option.text}
+                isDisabled={answers[index] !== undefined}
+                onSelect={() =>
+                  handleAnswer(index, option.is_correct, option.text)
+                }
+              />
+            ))}
+          </ul>
+          {answers[index] !== undefined && (
+            <p
+              className={`mt-3 text-right font-medium ${
+                answers[index] ? 'text-green-600' : 'text-red-600'
+              }`}
+            >
+              {answers[index] ? 'Resposta correta!' : 'Resposta incorreta.'}
+            </p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+interface QuestionOptionProps {
+  questionIndex: number;
+  optionIndex: number;
+  optionText: string;
+  isCorrect: boolean;
+  isSelected: boolean;
+  isDisabled: boolean;
+  onSelect: () => void;
+}
+
+function QuestionOption({
+  questionIndex,
+  optionIndex,
+  optionText,
+  isSelected,
+  isDisabled,
+  onSelect,
+}: QuestionOptionProps) {
+  return (
+    <li>
+      <label
+        htmlFor={`question-${questionIndex}-option-${optionIndex}`}
+        className={`flex cursor-pointer items-center gap-2 rounded-md border border-neutral-300 px-3 py-2 duration-100 ${isSelected ? 'border-blue-500 bg-blue-500/10' : 'hover:bg-blue-500/5'} ${isDisabled ? 'cursor-default opacity-60' : ''}`}
+      >
+        <input
+          id={`question-${questionIndex}-option-${optionIndex}`}
+          type="radio"
+          name={`question-${questionIndex}`}
+          value={optionText}
+          className="hidden"
+          onChange={onSelect}
+          disabled={isDisabled}
+          checked={isSelected}
+        />
+        {optionText}
+      </label>
+    </li>
+  );
+}
